1eec55700b13cb43d6681ed77f64350fa47e2286
shenqing <shenqin1@msu.edu>
2023-05-09 13:41:43 -0400

    Initial Commit

---
+++ IMDbMovies/IMDbMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDbMoviesApp.swift
+//  IMDbMovies
+//
+//  Created by 沈清昊 on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct IMDbMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
